0d668893d12896b50aefa3bb0f5bd230a23fe6ad
Pablo Rodriguez Carmona <p.rodriguez@merixstudio.com>
2018-07-05 15:27:15 +0200

    [#158820403] Refactor how test are writteng, Add new tests

--- medications/tests/test_models.py
+++ medications/tests/test_models.py
@@ -1,19 +1,44 @@
 import pytest
+
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from medications.factories import OrganizationFactory
 
+pytestmark = pytest.mark.django_db()
+ORGANIZATION_NAME = 'Test Organization'
 
-@pytest.mark.django_db
-def test_organization_model(django_user_model):
+
+class TestOrganization: 
     """ Test organization model """
-    user = django_user_model.objects.create(
-        email='example@example.com',
-        password='secretkey',
-    )
-    organization_name = 'Test Organization'
-    organization = OrganizationFactory(
-        user=user,
-        organization_name=organization_name,
 
-    )
-    assert organization.organization_name == organization_name
-    assert str(organization) == organization_name
+    def test_str(self):
+        organization = OrganizationFactory(
+            organization_name=ORGANIZATION_NAME,
+
+        )
+        assert organization.organization_name == ORGANIZATION_NAME
+        assert str(organization) == ORGANIZATION_NAME
+
+    def test_user(self, django_user_model):
+        user = django_user_model.objects.create(
+            email='example@example.com',
+            password='secretkey',
+        )
+        organization = OrganizationFactory(
+            user=user,
+            organization_name=ORGANIZATION_NAME,
+        )
+        assert isinstance(organization.user, get_user_model())
+
+    def test_phone(self):
+        # Test that only US phone are valid
+        organization_incorrect_phone = OrganizationFactory(
+            organization_name=ORGANIZATION_NAME,
+            phone='666-666-666'
+        )
+        organization_correct_phone = OrganizationFactory(
+            organization_name=ORGANIZATION_NAME,
+            phone='202-555-0178'
+        )
+        assert not organization_incorrect_phone.phone.is_valid()
+        assert organization_correct_phone.phone.is_valid()
