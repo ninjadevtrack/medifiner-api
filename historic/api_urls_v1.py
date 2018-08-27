fe67f9c430c1721aeff98f15bbe0959505553e8c
Pablo Rodriguez Carmona <p.rodriguez@merixstudio.com>
2018-08-27 10:51:14 +0200

    #159448884 Add endpoint to check average historic data for an specific zip code

--- historic/api_urls_v1.py
+++ historic/api_urls_v1.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
     HistoricAverageNationalLevelView,
     HistoricAverageStateLevelView,
+    HistoricAverageZipCodeLevelView,
 )
 
 historic_api_urlpatterns = [
@@ -13,5 +14,9 @@ historic_api_urlpatterns = [
     path(
         'average/state/<int:state_id>/',
         HistoricAverageStateLevelView.as_view(),
+    ),
+    path(
+        'average/zipcode/<str:zipcode>/',
+        HistoricAverageZipCodeLevelView.as_view(),
     ),
 ]
