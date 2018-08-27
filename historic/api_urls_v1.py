01d4eca7be34347edf9f11c6be9c461b633d176c
Pablo Rodriguez Carmona <p.rodriguez@merixstudio.com>
2018-08-27 10:36:24 +0200

    #159448884 Add endpoint to check average historic data for an specific state

--- historic/api_urls_v1.py
+++ historic/api_urls_v1.py
@@ -2,11 +2,16 @@ from django.urls import path
 
 from .views import (
     HistoricAverageNationalLevelView,
+    HistoricAverageStateLevelView,
 )
 
 historic_api_urlpatterns = [
     path(
         'average/',
         HistoricAverageNationalLevelView.as_view(),
+    ),
+    path(
+        'average/state/<int:state_id>/',
+        HistoricAverageStateLevelView.as_view(),
     ),
 ]
