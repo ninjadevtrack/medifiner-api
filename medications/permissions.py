23cb993bfd1776ceeae95e98cc23cd9f288dc221
Pablo Rodriguez Carmona <p.rodriguez@merixstudio.com>
2018-08-29 16:24:04 +0200

    #159867185 Create special permission for user state, user with state level permission only can see information about their state; Add same kind of permission for zipcode, a user can se only zipcodes attached to their state; Filter the states retrieved when getting the list of state to show only the state user belongs to

--- medications/permissions.py
+++ medications/permissions.py
@@ -12,3 +12,45 @@ class NationalLevel(permissions.BasePermission):
                 request.user.permission_level == request.user.NATIONAL_LEVEL
             )
         )
+
+
+class SelfStatePermissionLevel(permissions.BasePermission):
+    message = 'This user has not state level permission for this state'
+
+    def has_permission(self, request, view):
+        super_permission = (
+            request.method in permissions.SAFE_METHODS and (
+                request.user and
+                request.user.is_authenticated and
+                request.user.permission_level == request.user.NATIONAL_LEVEL
+            )
+        )
+        user_state = getattr(request.user, 'state', None)
+        view_state = view.kwargs.get('state_id')
+        state_permission = False
+        if user_state:
+            state_permission = user_state.id == view_state
+        return super_permission or state_permission
+
+
+class SelfZipCodePermissionLevel(permissions.BasePermission):
+    message = 'This user has no permission to check this zipcode'
+
+    def has_permission(self, request, view):
+        super_permission = (
+            request.method in permissions.SAFE_METHODS and (
+                request.user and
+                request.user.is_authenticated and
+                request.user.permission_level == request.user.NATIONAL_LEVEL
+            )
+        )
+        user_state = getattr(request.user, 'state', None)
+        user_zipcodes = []
+        if user_state:
+            user_zipcodes = user_state.state_zipcodes.values_list(
+                'zipcode',
+                flat=True,
+            )
+        view_zipcode = view.kwargs.get('zipcode')
+        zipcode_permission = view_zipcode in user_zipcodes
+        return super_permission or zipcode_permission
