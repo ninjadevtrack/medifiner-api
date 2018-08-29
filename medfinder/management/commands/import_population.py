25c089a3e8e90957962ff9706af7ba6a4a14cd0a
Pablo Rodriguez Carmona <p.rodriguez@merixstudio.com>
2018-08-29 17:04:06 +0200

    #159261271 Get the population information for state and county from the 2017 database; Create a validation for some cases when there is no information for a county in 2017, in that case request the information from the 2010 census which is more complete (only 3 cases were found)

--- medfinder/management/commands/import_population.py
+++ medfinder/management/commands/import_population.py
@@ -5,12 +5,14 @@ from django.core.management.base import BaseCommand, CommandError
 
 from medications.models import County, State, ZipCode
 
-
-URL_CENSUS = 'https://api.census.gov/data/2010/sf1'
-POPULATION_VARIABLE = 'P0010001'
+URL_CENSUS = 'https://api.census.gov/data/2017/pep/population'
+URL_CENSUS_2010 = 'https://api.census.gov/data/2010/sf1'
+POPULATION_VARIABLE = 'POP'
+POPULATION_VARIABLE_2010 = 'P0010001'
 
 REQUEST_STATE = '?get={population_variable}&for=state:{state_id}&key={api_key}' # noqa
 REQUEST_COUNTY = '?get={population_variable}&for=county:*&in=state:{state_id}&key={api_key}' # noqa
+SPECIAL_REQUEST_COUNTY = '?get={population_variable}&for=county:{county_id}&in=state:{state_id}&key={api_key}' # noqa
 REQUEST_ZIPCODE = '?get={population_variable}&for=zip%20code%20tabulation%20area:*&in=state:{state_id}&key={api_key}' # noqa
 
 
@@ -71,16 +73,35 @@ class Command(BaseCommand):
                 # county_pop_map
 
                 county_id = f'{county.county_id:03}' # noqa
-                population = int(county_pop_map.get(county_id))
+                try:
+                    population = int(county_pop_map.get(county_id))
+                except TypeError:
+                    # the 2017 has no data for some counties, in that case we
+                    # ask to the 2010 census for this special county
+                    print(
+                        'No data for {}, requesting data from the'
+                        ' 2010 census'.format(county)
+                    )
+                    request_for_county = SPECIAL_REQUEST_COUNTY.format(
+                        population_variable=POPULATION_VARIABLE_2010,
+                        county_id=county.county_id,
+                        state_id=state.state_us_id,
+                        api_key=settings.CENSUS_API_KEY,
+                    )
+                    full_path_request_for_county = \
+                        URL_CENSUS_2010 + request_for_county
+                    response = requests.get(full_path_request_for_county)
+                    county_data_response = response.json()
+                    population = int(county_data_response[1][0])
                 county.population = population
                 county.save()
 
             request_for_zipcode = REQUEST_ZIPCODE.format(
-                population_variable=POPULATION_VARIABLE,
+                population_variable=POPULATION_VARIABLE_2010,
                 state_id=state.state_us_id,
                 api_key=settings.CENSUS_API_KEY,
             )
-            full_path_request_for_zipcode = URL_CENSUS + request_for_zipcode # noqa
+            full_path_request_for_zipcode = URL_CENSUS_2010 + request_for_zipcode # noqa
             response = requests.get(full_path_request_for_zipcode)
             zipcode_data_response = response.json()
 
