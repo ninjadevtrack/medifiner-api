a9950905f13f75cea4297b62f4f80fba303d3f58
Pablo Rodriguez Carmona <p.rodriguez@merixstudio.com>
2018-08-31 14:35:55 +0200

    #159604062 Create view an urls for exporting the csv, Make possible one view to handle all the posible csv exports

--- medications/views.py
+++ medications/views.py
@@ -1,4 +1,6 @@
-from django.db.models import Q, Count
+from datetime import datetime, timedelta
+
+from django.db.models import Q, Count, Prefetch
 from django.contrib.postgres.aggregates import ArrayAgg
 from django.contrib.gis.db.models.functions import Centroid, AsGeoJSON
 from django.core.cache import cache
@@ -505,3 +507,143 @@ class MedicationTypesView(views.APIView):
                 Medication.DRUG_TYPE_CHOICES
             ).get(drug_type['drug_type'])
         return Response(values)
+
+
+class CSVExportView(GenericAPIView):
+    permission_classes = (AllowAny,)
+    allowed_methods = ['GET']
+
+    def dispatch(self, request, *args, **kwargs):
+        kwargs_keys = kwargs.keys()
+        if 'state_id' in kwargs_keys:
+            self.state_id = kwargs.pop('state_id')
+        if 'zipcode' in kwargs_keys:
+            self.zipcode = kwargs.pop('zipcode')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_queryset(self, state_id=None, zipcode=None):
+        med_id = self.request.query_params.get('med_id')
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
+        try:
+            if not med_id or int(
+                med_id
+            ) not in MedicationName.objects.values_list(
+                'id',
+                flat=True,
+            ):
+                return Medication.objects.none()
+        except ValueError:
+            return Medication.objects.none()
+
+        if not (start_date and end_date):
+            return Medication.objects.none()
+
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+
+        # First we take list of provider medication for this med, we will
+        # use it for future filters
+        if zipcode:
+            provider_medication_qs = ProviderMedicationThrough.objects.filter(
+                medication__medication_name__id=med_id,
+                provider__related_zipcode__zipcode=zipcode,
+            )
+        elif not zipcode and state_id:
+            provider_medication_qs = ProviderMedicationThrough.objects.filter(
+                medication__medication_name__id=med_id,
+                provider__related_zipcode__state=state_id,
+            )
+        else:
+            provider_medication_qs = ProviderMedicationThrough.objects.filter(
+                medication__medication_name__id=med_id,
+            )
+        # Now we check if there is a list of type of providers to filter
+        provider_type_list = self.request.query_params.get(
+            'provider_type',
+            [],
+        )
+        if provider_type_list:
+            try:
+                provider_type_list = provider_type_list.split(',')
+                provider_medication_qs = provider_medication_qs.filter(
+                    provider__type__in=provider_type_list,
+                )
+            except ValueError:
+                pass
+
+        # We take the formulation ids and transform them to use like filter
+        formulation_ids_raw = self.request.query_params.get(
+            'formulations',
+        )
+        formulation_ids = []
+        if formulation_ids_raw:
+            try:
+                formulation_ids = list(
+                    map(int, formulation_ids_raw.split(','))
+                )
+            except ValueError:
+                pass
+        if formulation_ids:
+            provider_medication_qs = provider_medication_qs.filter(
+                medication__id__in=formulation_ids,
+            )
+
+        # Now we check if there is a list of category of providers to filter
+        provider_category_list = self.request.query_params.get(
+            'provider_category',
+            [],
+        )
+        if provider_category_list:
+            try:
+                provider_category_list = provider_category_list.split(',')
+                provider_medication_qs = provider_medication_qs.filter(
+                    provider__category__in=provider_category_list,
+                )
+            except ValueError:
+                pass
+
+        # Now we check if there is a list of drug types to filter
+        drug_type_list = self.request.query_params.get(
+            'drug_type',
+            [],
+        )
+        if drug_type_list:
+            try:
+                drug_type_list = drug_type_list.split(',')
+                provider_medication_qs = provider_medication_qs.filter(
+                    medication__drug_type__in=drug_type_list,
+                )
+            except ValueError:
+                pass
+
+        provider_medication_ids = provider_medication_qs.values_list(
+            'id',
+            flat=True,
+        )
+        qs = Medication.objects.filter(
+            provider_medication__id__in=provider_medication_ids,
+        ).prefetch_related(
+            Prefetch(
+                'provider_medication',
+                queryset=ProviderMedicationThrough.objects.filter(
+                    date__gte=start_date,
+                    date__lte=end_date + timedelta(days=1),
+                    # We add 1 day cause we want to check the medications
+                    # supplies in end_date until 23:59:59 which in practice
+                    # is next day 00:00:00
+                )
+            )
+        ).distinct()
+        return qs
+
+    def get(self, request):
+
+        qs = self.get_queryset(
+            getattr(self, 'state_id', None),
+            getattr(self, 'zipcode', None),
+        )
+        # TODO generate the CSV, queryset already done
+        # return the csv
+        return None
+
