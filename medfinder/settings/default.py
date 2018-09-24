aae1fc7f5510f7f707b1fad1b6e349235c2ffc0b
Pablo Rodriguez Carmona <p.rodriguez@merixstudio.com>
2018-09-24 10:08:43 +0200

    Remove repeated old code

--- medfinder/settings/default.py
+++ medfinder/settings/default.py
@@ -185,16 +185,6 @@ if S3_STORAGE_ENABLE:
 # --- DJANGO COMPRESSOR ---
 # STATICFILES_FINDERS += ('compressor.finders.CompressorFinder',)
 
-# --- CACHE ---
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
 # --- DJANGO REGISTRATION REDUX ---
 ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = False
