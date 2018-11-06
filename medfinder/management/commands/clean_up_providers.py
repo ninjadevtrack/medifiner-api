9bb2177bd8e22d2c86c29db2679ebcd4a848f2f2
alexrubinstien <alrubinstien1088@gmail.com>
2018-11-06 14:03:41 +0100

    fix clean up providers script

--- medfinder/management/commands/clean_up_providers.py
+++ medfinder/management/commands/clean_up_providers.py
@@ -13,17 +13,29 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for provider in Provider.objects.exclude(name='').all():
-            providers_to_delete = Provider.objects.filter(
+            no_name_providers = Provider.objects.filter(
                 name='', store_number=provider.store_number)
-            if len(providers_to_delete) == 1:
-                provider_to_delete = providers_to_delete[0]
-                provider.related_zipcode_id = provider_to_delete.related_zipcode_id
-                provider.save()
+            if len(no_name_providers) == 1:
+                no_name_provider = no_name_providers[0]
+                no_name_provider.address = provider.address
+                no_name_provider.city = provider.city
+                no_name_provider.email = provider.email
+                no_name_provider.end_date = provider.end_date
+                no_name_provider.insurance_accepted == provider.insurance_accepted
+                no_name_provider.name = provider.name
+                no_name_provider.notes = provider.notes
+                no_name_provider.operating_hours = provider.operating_hours
+                no_name_provider.phone = provider.phone
+                no_name_provider.related_zipcode_id = provider.related_zipcode_id
+                no_name_provider.start_date = provider.start_date
+                no_name_provider.state = provider.state
+                no_name_provider.store_number = provider.store_number
+                no_name_provider.type_id = provider.type_id
+                no_name_provider.website = provider.website
+                no_name_provider.walkins_accepted = provider.walkins_accepted
+                no_name_provider.zip = provider.zip
+                no_name_provider.save()
 
-                ProviderMedicationNdcThrough.objects.filter(
-                    provider_id=provider_to_delete.pk).update(provider_id=provider.pk)
-
-                provider_to_delete.delete()
-            elif len(providers_to_delete) > 1:
+            elif len(no_name_providers) > 1:
                 print("WILL NOT PROCESS PROVIDER ID")
                 print(provider.pk)
