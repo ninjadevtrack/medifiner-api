4d2c1761d5444e599ababcc8e168af6c3de2cbdb
Pablo Rodriguez Carmona <p.rodriguez@merixstudio.com>
2018-11-12 18:28:59 +0100

    test code

--- historic/serializers.py
+++ historic/serializers.py
@@ -10,23 +10,61 @@ from medications.utils import get_supplies
 from .utils import daterange, get_overall
 
 
-class AverageSupplyLevelListSerializer(serializers.ListSerializer):
+class AverageSupplyLevelSerializer(serializers.Serializer):
 
-    @property
-    def data(self):
-        return super(serializers.ListSerializer, self).data
 
     def to_representation(self, data):
+        medication_data = []
+        medication_ids = data.values_list('medication_ndc_id', flat=True)
+
+        date_format = '%Y-%m-%d'
+        start_date = self.context[
+            'request'
+        ].query_params.get('start_date')
+        start_date = datetime.datetime.strptime(
+            start_date,
+            date_format,
+        )
+
+        end_date = self.context[
+            'request'
+        ].query_params.get('end_date')
+        end_date = datetime.datetime.strptime(
+            end_date,
+            date_format,
+        )
+
+        for medication_id in medication_ids:
+            days = []
+            filtered_qs = data.filter(medication_ndc_id=medication_id)
+            medication_name = data.first().get('medication_ndc__medication__name')
+            for date in daterange(start_date, end_date, inclusive=True):
+                supply_levels = []
+                for provider_medication in filtered_qs:
+                    if provider_medication.get(
+                        'creation_date',
+                    ).day == date.day:
+                        supply_levels.append(provider_medication.get('level'))
+                days.append(
+                    {
+                        'day': date.date(),
+                        'supply': get_supplies(supply_levels),
+                    }
+                )
+            medication_data.append({
+                'name': medication_name, 'average_supply_per_day': days
+            })
+
         return OrderedDict((
-            ('medication_supplies', super().to_representation(data)),
+            ('medication_supplies', medication_data),
         ))
 
 
 class AverageSupplyLevelZipCodeListSerializer(serializers.ListSerializer):
 
-    @property
-    def data(self):
-        return super(serializers.ListSerializer, self).data
+    # @property
+    # def data(self):
+        # return super(serializers.ListSerializer, self).data
 
     def to_representation(self, data):
         zipcode = self.context['request'].data.get('zipcode')
@@ -44,52 +82,51 @@ class AverageSupplyLevelZipCodeListSerializer(serializers.ListSerializer):
             ))
 
 
-class AverageSupplyLevelSerializer(serializers.ModelSerializer):
-    average_supply_per_day = serializers.SerializerMethodField()
+# class AverageSupplyLevelSerializer(serializers.ModelSerializer):
+#     # average_supply_per_day = serializers.SerializerMethodField()
 
-    class Meta:
-        model = Medication
-        list_serializer_class = AverageSupplyLevelListSerializer
-        fields = (
-            'name',
-            'average_supply_per_day',
-        )
+#     class Meta:
+#         list_serializer_class = AverageSupplyLevelListSerializer
+#         fields = (
+#             'name',
+            # 'average_supply_per_day',
+        # )
 
-    def get_average_supply_per_day(self, obj):
-        provider_medication_qs = []
-        for ndc_code in obj.ndc_codes.all():
-            for provider_medication in ndc_code.provider_medication.all():
-                provider_medication_qs.append(provider_medication)
-        days = []
-        date_format = '%Y-%m-%d'
-        start_date = self.context[
-            'request'
-        ].query_params.get('start_date')
-        start_date = datetime.datetime.strptime(
-            start_date,
-            date_format,
-        )
+    # def get_average_supply_per_day(self, obj):
+    #     provider_medication_qs = []
+    #     for ndc_code in obj.ndc_codes.all():
+    #         for provider_medication in ndc_code.provider_medication.all():
+    #             provider_medication_qs.append(provider_medication)
+    #     days = []
+    #     date_format = '%Y-%m-%d'
+    #     start_date = self.context[
+    #         'request'
+    #     ].query_params.get('start_date')
+    #     start_date = datetime.datetime.strptime(
+    #         start_date,
+    #         date_format,
+    #     )
 
-        end_date = self.context[
-            'request'
-        ].query_params.get('end_date')
-        end_date = datetime.datetime.strptime(
-            end_date,
-            date_format,
-        )
-        for date in daterange(start_date, end_date, inclusive=True):
-            supply_levels = []
-            if provider_medication_qs:
-                for provider_medication in provider_medication_qs:
-                    if provider_medication.creation_date.day == date.day:
-                        supply_levels.append(provider_medication.level)
-            days.append(
-                {
-                    'day': date.date(),
-                    'supply': get_supplies(supply_levels),
-                }
-            )
-        return days
+    #     end_date = self.context[
+    #         'request'
+    #     ].query_params.get('end_date')
+    #     end_date = datetime.datetime.strptime(
+    #         end_date,
+    #         date_format,
+    #     )
+    #     for date in daterange(start_date, end_date, inclusive=True):
+    #         supply_levels = []
+    #         if provider_medication_qs:
+    #             for provider_medication in provider_medication_qs:
+    #                 if provider_medication.creation_date.day == date.day:
+    #                     supply_levels.append(provider_medication.level)
+    #         days.append(
+    #             {
+    #                 'day': date.date(),
+    #                 'supply': get_supplies(supply_levels),
+    #             }
+    #         )
+        # return days
 
 
 class AverageSupplyLevelZipCodeSerializer(AverageSupplyLevelSerializer):
@@ -108,7 +145,7 @@ class OverallSupplyLevelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = MedicationName
-        list_serializer_class = AverageSupplyLevelListSerializer
+        # list_serializer_class = AverageSupplyLevelListSerializer
         fields = (
             'overall_supply_per_day',
         )
--- historic/views.py
+++ historic/views.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta
 
-from django.db.models import Prefetch
+from django.db.models import Prefetch, Count
+from rest_framework.response import Response
 from django.utils.translation import ugettext_lazy as _
 
 from rest_registration.exceptions import BadRequest
 
+from rest_framework.views import APIView
 from rest_framework.generics import (
     ListAPIView,
 )
@@ -113,12 +115,12 @@ def get_provider_medication_queryset(query_params, state_id=None, zipcode=None):
     return provider_medication_ids
 
 
-class HistoricAverageNationalLevelView(ListAPIView):
-    serializer_class = AverageSupplyLevelSerializer
+class HistoricAverageNationalLevelView(APIView):
+    # serializer_class = AverageSupplyLevelSerializer
     permission_classes = (IsAuthenticated,)
     allowed_methods = ['GET']
 
-    def get_queryset(self):
+    def get(self, request):
         '''
         query_params:
             - med_id: MedicationName id
@@ -129,10 +131,10 @@ class HistoricAverageNationalLevelView(ListAPIView):
             - provider_type: list of ProviderType ids
             - drug_type: list of 1 character str, for drug_type in Medication
         '''
-        med_id = self.request.query_params.get('med_id')
-        query_params = self.request.query_params
-        start_date = self.request.query_params.get('start_date')
-        end_date = self.request.query_params.get('end_date')
+        med_id = request.query_params.get('med_id')
+        query_params = request.query_params
+        start_date = request.query_params.get('start_date')
+        end_date = request.query_params.get('end_date')
 
         try:
             if not med_id or int(
@@ -155,21 +157,19 @@ class HistoricAverageNationalLevelView(ListAPIView):
         provider_medication_ids = get_provider_medication_queryset(
             query_params,
         )
-        qs = Medication.objects.filter(
-            ndc_codes__provider_medication__id__in=provider_medication_ids,
-        ).prefetch_related(
-            Prefetch(
-                'ndc_codes__provider_medication',
-                queryset=ProviderMedicationNdcThrough.objects.filter(
-                    creation_date__gte=start_date,
-                    creation_date__lte=end_date + timedelta(days=1),
-                    # We add 1 day cause we want to check the medications
-                    # supplies in end_date until 23:59:59 which in practice
-                    # is next day 00:00:00
-                ),
-            )
-        ).distinct()
-        return qs
+        qs = ProviderMedicationNdcThrough.objects.filter(
+            id__in=provider_medication_ids,
+            creation_date__gte=start_date,
+            creation_date__lte=end_date + timedelta(days=1),
+        ).values(
+            'medication_ndc_id',
+            'medication_ndc__medication__name',
+            'level',
+            'creation_date',
+        )
+        context = {'request': request}
+        data = AverageSupplyLevelSerializer(context=context).to_representation(qs)
+        return Response(data)
 
 
 class HistoricAverageStateLevelView(ListAPIView):
@@ -227,7 +227,7 @@ class HistoricAverageStateLevelView(ListAPIView):
                 queryset=ProviderMedicationNdcThrough.objects.filter(
                     creation_date__gte=start_date,
                     creation_date__lte=end_date + timedelta(days=1),
-                )
+                ).only('creation_date', 'level'),
             )
         ).distinct()
         return qs
