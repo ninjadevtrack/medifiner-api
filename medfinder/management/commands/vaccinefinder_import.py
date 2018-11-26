018a09bda1312a6ad452406a0806401032356ff5
alexrubinstien <alrubinstien1088@gmail.com>
2018-11-26 11:04:15 -0800

    prevent script from failing if org creation error

--- medfinder/management/commands/vaccinefinder_import.py
+++ medfinder/management/commands/vaccinefinder_import.py
@@ -37,13 +37,17 @@ class Command(BaseCommand):
             'vaccinedb').exclude(pk=walgreens_id).all()
 
         for vaccine_finder_org in vaccine_finder_orgs:
-            organization, created = Organization.objects.using(
-                'default').get_or_create(
-                contact_name=vaccine_finder_org.contact_name,
-                organization_name=vaccine_finder_org.organization_name,
-                phone=vaccine_finder_org.phone,
-                website=vaccine_finder_org.website
-            )
+            try:
+                organization, created = Organization.objects.using(
+                    'default').get_or_create(
+                    contact_name=vaccine_finder_org.contact_name,
+                    organization_name=vaccine_finder_org.organization_name,
+                    phone=vaccine_finder_org.phone,
+                    website=vaccine_finder_org.website
+                )
+            except:
+                print("Organization could not be created")
+                continue
 
             if vaccine_finder_org.vfproviders.exclude(store_number__isnull=True).count() == organization.providers.count():
                 print(organization.organization_name + " already imported")
@@ -55,42 +59,46 @@ class Command(BaseCommand):
             ))
 
             count = 0
-            if vaccine_finder_org.vfproviders.exclude(store_number__in=already_imported_store_numbers).count() == 0:
+            if vaccine_finder_org.vfproviders.exclude(store_number__in=already_imported_store_numbers, store_number__isnull=True).count() == 0:
                 print(organization.organization_name + " already imported")
                 continue
 
             for vaccine_finder_provider in vaccine_finder_org.vfproviders.exclude(store_number__in=already_imported_store_numbers):
                 if vaccine_finder_provider.store_number and vaccine_finder_provider.store_number != '':
-                    provider = Provider.objects.create(
-                        address=vaccine_finder_provider.address,
-                        city=vaccine_finder_provider.city,
-                        email=vaccine_finder_provider.email,
-                        end_date=vaccine_finder_provider.end_date,
-                        insurance_accepted=(
-                            True if vaccine_finder_provider.insurance_accepted == 'Y' else False),
-                        lat=vaccine_finder_provider.lat,
-                        lng=vaccine_finder_provider.lon,
-                        name=vaccine_finder_provider.name,
-                        notes=vaccine_finder_provider.notes,
-                        operating_hours=vaccine_finder_provider.operating_hours,
-                        organization=organization,
-                        phone=vaccine_finder_provider.phone,
-                        relate_related_zipcode=True,
-                        start_date=vaccine_finder_provider.start_date,
-                        state=vaccine_finder_provider.state,
-                        store_number=vaccine_finder_provider.store_number,
-                        type=provider_type,
-                        website=vaccine_finder_provider.website,
-                        active=False,
-                        walkins_accepted=(
-                            True if vaccine_finder_provider.walkins_accepted == 'Y' else False),
-                        zip=vaccine_finder_provider.zip,
-                    )
                     count += 1
+                    try:
+                        provider = Provider.objects.create(
+                            address=vaccine_finder_provider.address,
+                            city=vaccine_finder_provider.city,
+                            email=vaccine_finder_provider.email,
+                            end_date=vaccine_finder_provider.end_date,
+                            insurance_accepted=(
+                                True if vaccine_finder_provider.insurance_accepted == 'Y' else False),
+                            lat=vaccine_finder_provider.lat,
+                            lng=vaccine_finder_provider.lon,
+                            name=vaccine_finder_provider.name,
+                            notes=vaccine_finder_provider.notes,
+                            operating_hours=vaccine_finder_provider.operating_hours,
+                            organization=organization,
+                            phone=vaccine_finder_provider.phone,
+                            relate_related_zipcode=True,
+                            start_date=vaccine_finder_provider.start_date,
+                            state=vaccine_finder_provider.state,
+                            store_number=vaccine_finder_provider.store_number,
+                            type=provider_type,
+                            website=vaccine_finder_provider.website,
+                            active=False,
+                            walkins_accepted=(
+                                True if vaccine_finder_provider.walkins_accepted == 'Y' else False),
+                            zip=vaccine_finder_provider.zip,
+                        )
+                    except:
+                        print('Provider could not be created')
                     print(count)
 
             print("------------------------------------")
             print("Imported")
             print(vaccine_finder_org.organization_name)
             print(count)
-            time.sleep(5)
+            if count > 0:
+                time.sleep(5)
