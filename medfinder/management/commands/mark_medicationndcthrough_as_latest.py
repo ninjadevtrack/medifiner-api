a8e5e3c2639695a6f8ebe37719e41720174137ad
alexrubinstien <alrubinstien1088@gmail.com>
2018-11-27 13:27:57 -0800

    adding status to mark_medicationndcthrough_as_latest.py

--- medfinder/management/commands/mark_medicationndcthrough_as_latest.py
+++ medfinder/management/commands/mark_medicationndcthrough_as_latest.py
@@ -15,14 +15,15 @@ class Command(BaseCommand):
     help = 'Import from Vaccine Finder DB to provider type'
 
     def handle(self, *args, **options):
+        print("STARTING mark_medicationndcthrough_as_latest")
         self.mark_medicationndcthrough_as_latest()
 
     def mark_medicationndcthrough_as_latest(self):
         provider_medication_ndcs = ProviderMedicationNdcThrough.objects.filter(
             latest=True)
 
-        print(provider_medication_ndcs.count())
-
+        provider_count = provider_medication_ndcs.count()
+        index = 0
         for provider_medication_ndc in provider_medication_ndcs:
             medication_ndc_id = provider_medication_ndc.medication_ndc_id
             provider_id = provider_medication_ndc.provider_id
@@ -35,3 +36,6 @@ class Command(BaseCommand):
 
             latest_entry.latest = True
             latest_entry.save()
+
+            index = index + 1
+            print(str(index) + '/' + str(provider_count))
