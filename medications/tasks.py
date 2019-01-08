a44a942b65e020c529a5f0db206b6201839698b3
alexrubinstien <alrubinstien1088@gmail.com>
2019-01-07 17:19:03 -0800

    huge performance improvement + create new report for missing providers

--- medications/tasks.py
+++ medications/tasks.py
@@ -42,14 +42,127 @@ from .models import (
 # This task can't be atomic because we need to run a post_save signal for
 # every ProviderMedicationThrough object created
 def generate_medications(cache_key, organization_id, email_to, import_date=False):
+
+    def get_provider_id(store_data, organization_id, provider_map):
+        store_number = store_data['store_number']
+
+        if str(store_number) in provider_map:
+            return provider_map[str(store_number)]
+
+        provider, created = Provider.objects.get_or_create(
+            defaults=store_data,
+            organization_id=organization_id,
+            store_number=store_number,
+        )
+
+        if created:
+            try:
+                zipcode_obj = ZipCode.objects.get(zipcode=store_data['zip'])
+            except MultipleObjectsReturned:
+                zipcode_obj = ZipCode.objects.filter(
+                    zipcode=store_data['zip'])[0]
+            except ZipCode.DoesNotExist:
+                zipcode_obj = None
+
+            if zipcode_obj:
+                provider.related_zipcode = zipcode_obj
+                provider.save()
+
+        return provider.id
+
+    def find_missing_medication_ids(medication_data, medication_ndc_map, ndc_to_medication_map, medication_id_to_ndc_code_map):
+        all_med_ids = list(set(ndc_to_medication_map.values()))
+
+        medication_ids = []
+        for data in medication_data:
+            medication_ids.append(
+                ndc_to_medication_map[find_medication_ndc_id(data, medication_ndc_map)])
+
+        for medication_id in medication_ids:
+            all_med_ids.remove(medication_id)
+
+        return all_med_ids
+
+    def add_no_report_entries(medication_data, missing_medication_ids, medication_id_to_ndc_code_map):
+        for missing_medication_id in missing_medication_ids:
+            medication_data.append({
+                'level': -1,
+                'ndc_code': medication_id_to_ndc_code_map[missing_medication_id],
+                'supply': 'NO REPORT',
+            })
+        return medication_data
+
+    def prepare_medication_data(provider_id, medication_data, number_of_medication_to_create, import_date, medication_ndc_map, ndc_to_medication_map, medication_id_to_ndc_code_map):
+        if len(medication_data) != number_of_medication_to_create:
+            missing_medication_ids = find_missing_medication_ids(
+                medication_data, medication_ndc_map, ndc_to_medication_map, medication_id_to_ndc_code_map)
+            medication_data = add_no_report_entries(
+                medication_data, missing_medication_ids, medication_id_to_ndc_code_map)
+
+        provider_medication_ndc_throughs = build_ProviderMedicationNdcThrough_objects(
+            provider_id, medication_data, import_date, medication_ndc_map)
+
+        return provider_medication_ndc_throughs
+
+    def find_medication_ndc_id(data, medication_ndc_map):
+        if data['ndc_code'] in medication_ndc_map:
+            return medication_ndc_map[data['ndc_code']]
+        else:
+            return False
+
+    def build_ProviderMedicationNdcThrough_objects(provider_id, medication_data, import_date, medication_ndc_map):
+        objects = []
+
+        for data in medication_data:
+            objects.append(build_ProviderMedicationNdcThrough_object(
+                data['supply'], data['level'], provider_id, find_medication_ndc_id(data, medication_ndc_map), import_date))
+
+        return objects
+
+    def build_ProviderMedicationNdcThrough_object(supply, level, provider_id, medication_ndc_id, import_date):
+        if medication_ndc_id:
+            if import_date:
+                return ProviderMedicationNdcThrough(
+                    creation_date=import_date,
+                    latest=False,
+                    last_modified=import_date,
+                    level=level,
+                    medication_ndc_id=medication_ndc_id,
+                    provider_id=provider_id,
+                    supply=supply,
+                )
+            else:
+                now = timezone.now()
+                return ProviderMedicationNdcThrough(
+                    creation_date=now,
+                    latest=True,
+                    last_modified=now,
+                    level=level,
+                    medication_ndc_id=medication_ndc_id,
+                    provider_id=provider_id,
+                    supply=supply,
+                )
+
+    def prepare_current_store_data(store_data, medication_data, organization_id, number_of_medication_to_create, import_date, medication_ndc_map, ndc_to_medication_map, medication_id_to_ndc_code_map, provider_map):
+        provider_id = get_provider_id(
+            store_data, organization_id, provider_map)
+
+        provider_medication_ndc_throughs = prepare_medication_data(provider_id, medication_data,
+                                                                   number_of_medication_to_create, import_date, medication_ndc_map, ndc_to_medication_map, medication_id_to_ndc_code_map)
+
+        return provider_medication_ndc_throughs, provider_id
+
+    def batch_create_provider_medication_ndc_throughs(provider_medication_ndc_throughs):
+        ProviderMedicationNdcThrough.objects.bulk_create(
+            provider_medication_ndc_throughs)
+
     beginning_time = timezone.now()
-    # Already checked in serializer validation that this organization exists.
-    lost_ndcs = []
 
     # A list to update the last_import_date field in
     # all providers during this import
     updated_providers = []
 
+    number_of_medication_to_create = Medication.objects.count()
     organization = Organization.objects.get(pk=organization_id)
     existing_ndc_codes = ExistingMedication.objects.values_list(
         'ndc',
@@ -58,100 +171,82 @@ def generate_medications(cache_key, organization_id, email_to, import_date=False
     provider = None
     medication_ndc = None
     medication_ndc_map = {}  # Use this map to save queries to the DB
+    ndc_to_medication_map = {}
+    medication_id_to_ndc_code_map = {}
+
+    for ndc_entry in MedicationNdc.objects.all():
+        medication_ndc_map[ndc_entry.ndc] = ndc_entry.id
+        ndc_to_medication_map[ndc_entry.id] = ndc_entry.medication_id
+        medication_id_to_ndc_code_map[ndc_entry.medication_id] = ndc_entry.ndc
+
+    provider_map = {}  # Use this map to save queries to the DB
+
+    for provider in Provider.objects.filter(organization_id=organization_id):
+        provider_map[str(provider.store_number)] = provider.id
+
     csv_file = cache.get(cache_key)
     temporary_file_obj = csv_file.open()
     decoded_file = temporary_file_obj.read().decode('utf-8').splitlines()
     reader = csv.DictReader(decoded_file)
+    sorted_csv = sorted(reader, key=lambda d: float(d['store #']))
     index = 0
-    for row in reader:
+
+    currently_processing_store_number = False
+    current_store_data = {}
+    current_store_medications_data = {}
+
+    provider_medication_ndc_throughs_to_batch_insert = []
+
+    supply_to_level_map = {
+        'NO REPORT': -1,
+        'NO SUPPLY': 0,
+        '<24': 1,
+        '24': 2,
+        '24-48': 3,
+        '>48': 4,
+    }
+
+    for row in sorted_csv:
         index += 1
-        if index % 30000 == 0:
-            sleep(30)
-        # Iterate through the rows to get the neccessary information
+
         store_number = row.get('store #')
-        address = row.get('address')
-        city = row.get('city')
-        zip_code = row.get('zipcode')
-        state = row.get('state')
-        phone = row.get('phone')
-        ndc_code = row.get('med_code')
-        med_name = row.get('med_name')
+        supply_level = row.get('supply_level')
+        level = supply_to_level_map.get(supply_level, 0)
 
-        # First check if the ndc code exists in real life, if not, don't use
-        # unnecesary time in meaningless queries
-        if ndc_code in existing_ndc_codes:
-            if not provider or (
-                provider and provider.store_number != store_number
-            ):
-                # Check if we already have a provider cached and if it is the
-                # same as the next in line to avoid too much queries to the DB
-                # and try to get the zipcode object to relate it to provider.
-                try:
-                    zipcode_obj = ZipCode.objects.get(zipcode=zip_code)
-                except MultipleObjectsReturned:
-                    zipcode_obj = ZipCode.objects.filter(zipcode=zip_code)[0]
-                except ZipCode.DoesNotExist:
-                    zipcode_obj = None
+        current_medication_data = {
+            'level': level,
+            'ndc_code': row.get('med_code'),
+            'supply': supply_level,
+        }
 
-                # TODO: type and category? Home delivery info?
-                default_provider_data = {'phone': phone, 'city': city, 'state': state,
-                                         'address': address, 'zip': zip_code, 'related_zipcode': zipcode_obj}
-                provider, created = Provider.objects.get_or_create(
-                    organization=organization,
-                    store_number=store_number,
-                    defaults=default_provider_data
-                )
-
-            if not medication_ndc or (medication_ndc.ndc != ndc_code):
-                # Will do the same check as in provider
-                # Second check in the medication_map to lookup if this
-                # medication has been created already from this file
-                medication_ndc = medication_ndc_map.get(ndc_code, None)
-                if not medication_ndc:
-                    try:
-                        medication_ndc = \
-                            MedicationNdc.objects.get(
-                                ndc=ndc_code
-                            )
-                    except (
-                        IntegrityError,
-                        MedicationNdc.DoesNotExist,
-                    ):
-                        lost_ndcs.append((med_name, ndc_code))
-                        pass
-
-            if medication_ndc:
-                # Add the actual medication (whatever it is) to the medication
-                # map. We will use as key the ndc which is supossed to be a
-                # real life id which should be unique, the value will be the
-                # object that we can get. Python get from dict uses much
-                # less programmatic time than a SQL get.
-
-                medication_ndc_map[medication_ndc.ndc] = medication_ndc
-            if provider and medication_ndc:
-                # Create or update the relation object
-                if import_date:
-                    ProviderMedicationNdcThrough.objects.create(
-                        provider=provider,
-                        medication_ndc=medication_ndc,
-                        supply=row.get('supply_level'),
-                        latest=False,
-                        creation_date=import_date,
-                        last_modified=import_date,
-                    )
-                else:
-                    ProviderMedicationNdcThrough.objects.create(
-                        provider=provider,
-                        medication_ndc=medication_ndc,
-                        supply=row.get('supply_level'),
-                        latest=True
-                    )
-                if provider not in updated_providers:
-                    updated_providers.append(provider.id)
+        if currently_processing_store_number == store_number:
+            current_store_medications_data.append(current_medication_data)
         else:
-            lost_ndcs.append((med_name, ndc_code))
+            if currently_processing_store_number:
+                provider_medication_ndc_throughs, provider_id = prepare_current_store_data(
+                    current_store_data, current_store_medications_data, organization_id, number_of_medication_to_create, import_date, medication_ndc_map, ndc_to_medication_map, medication_id_to_ndc_code_map, provider_map)
 
-    # Finnally update the last_import_date in all the updated_providers
+                provider_medication_ndc_throughs_to_batch_insert = provider_medication_ndc_throughs_to_batch_insert + \
+                    provider_medication_ndc_throughs
+
+                if provider_id not in updated_providers:
+                    updated_providers.append(provider_id)
+
+            currently_processing_store_number = store_number
+            current_store_medications_data = [current_medication_data]
+            current_store_data = {
+                'address': row.get('address'),
+                'city': row.get('city'),
+                'phone': row.get('phone'),
+                'state': row.get('state'),
+                'store_number': store_number,
+                'zip': row.get('zipcode'),
+            }
+
+    batch_create_provider_medication_ndc_throughs(
+        provider_medication_ndc_throughs_to_batch_insert)
+
+    # Finally update the last_import_date in all the updated_providers
     if updated_providers:
         Provider.objects.filter(
             id__in=updated_providers,
@@ -167,32 +262,19 @@ def generate_medications(cache_key, organization_id, email_to, import_date=False
     if email_to:
         finnish_time = timezone.now()
         duration = finnish_time - beginning_time
-        duration_minutes = round(duration.seconds / 60, 0)
+        duration_seconds = duration.seconds
         tz = timezone.pytz.timezone('EST')
         est_finnish_time = datetime.now(tz)
 
-        if lost_ndcs:
-            msg_plain = (
-                'Completion date time: {}\n'
-                'Duration: {} minutes\n'
-                'Status: {} rows processed\n'
-                '{} CSV entries were not imported\n'
-            ).format(
-                est_finnish_time.strftime('%Y-%m-%d %H:%M'),
-                duration_minutes,
-                index,
-                len(lost_ndcs),
-            )
-        else:
-            msg_plain = (
-                'Completion date time: {}\n'
-                'Duration: {} minutes\n'
-                'Status: {} CSV rows correctly imported.\n'
-            ).format(
-                est_finnish_time.strftime('%Y-%m-%d %H:%M'),
-                duration_minutes,
-                index,
-            )
+        msg_plain = (
+            'Completion date time: {}\n'
+            'Duration: {} seconds\n'
+            'Status: {} CSV rows correctly imported.\n'
+        ).format(
+            est_finnish_time.strftime('%Y-%m-%d %H:%M'),
+            duration_seconds,
+            index,
+        )
         send_mail(
             'MedFinder Import Status',
             msg_plain,
