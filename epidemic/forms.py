6148546b38a810742221617cd6392c235968fbad
pioneeringdev <codedec116@gmail.com>
2019-01-22 00:12:22 +0800

    implement Alert Banner content length validation with trimming html tags

--- epidemic/forms.py
+++ epidemic/forms.py
@@ -12,13 +12,26 @@
 
 from django import forms
 from tinymce.widgets import TinyMCE
-
+import re
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
 class EpidemicForm(forms.ModelForm):
 
-  content = forms.CharField(max_length=300, required=False,  widget=TinyMCE(attrs={'cols':80, 'rows': 4}, mce_attrs={   
+  content = forms.CharField(required=False, widget=TinyMCE(attrs={'cols':80, 'rows': 4}, mce_attrs={   
       "theme": "advanced",
       "plugins": "table,spellchecker,paste,searchreplace",
       'max_chars': "300",         
      
   }))
   active = forms.BooleanField()  
+
+  def clean_content(self):
+        content = self.cleaned_data['content']
+        html_tag_trim = re.compile('<.*?>')
+        trimedContent =re.sub(html_tag_trim, '', content); 
+        length = len(trimedContent)
+        if len(trimedContent) > 300:
+            raise forms.ValidationError(u"The maximum length of the content is 300. You have entered "+ str(length) +" characters.")
+        return content
